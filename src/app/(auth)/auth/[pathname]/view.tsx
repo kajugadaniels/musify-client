b5b9d2a48067e3e74f8c07229fc749afafcd6297
kajugadaniels <kajugadaniels@gmail.com>
2025-11-06 00:01:15 +0200

    Change the versions

--- src/app/(auth)/auth/[pathname]/view.tsx
+++ src/app/(auth)/auth/[pathname]/view.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AccountsCard } from "@daveyplate/better-auth-ui";
+import { AuthCard } from "@daveyplate/better-auth-ui";
 import { ArrowLeftIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Button } from "~/components/ui/button";
@@ -9,7 +9,7 @@ export function AuthView({ pathname }: { pathname: string }) {
     const router = useRouter();
 
     return (
-        <main className="container flex grow flex-col items-center justify-center gap-3 p-4 md:p-6">
+        <main className="container flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6">
             {["settings", "security"].includes(pathname) && (
                 <Button
                     className="self-start"
@@ -20,8 +20,7 @@ export function AuthView({ pathname }: { pathname: string }) {
                     Back
                 </Button>
             )}
-
-            <AccountsCard pathname={pathname} />
+            <AuthCard pathname={pathname} />
         </main>
     );
 }
