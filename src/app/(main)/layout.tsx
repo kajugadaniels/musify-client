5f11d69c92e369a59573b5a7737bbe9d6110b06a
kajugadaniels <kajugadaniels@gmail.com>
2025-11-05 18:17:30 +0200

    Change app name

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,11 +4,27 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Providers } from "~/components/providers";
 import { Toaster } from "~/components/ui/sonner";
-import { SidebarProvider } from "~/components/ui/sidebar";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "~/components/ui/sidebar";
+import { AppSidebar } from "~/components/sidebar/app-sidebar";
+import { Separator } from "~/components/ui/separator";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+import BreadcrumbPageClient from "~/components/sidebar/breadcrumb-page-client";
+// import SoundBar from "~/components/sound-bar";
 
 export const metadata: Metadata = {
-    title: "Musify",
-    description: "Music Generator App",
+    title: "Home",
+    description: "Music Generator",
     icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
@@ -24,9 +40,30 @@ export default function RootLayout({
         <html lang="en" className={`${geist.variable}`}>
             <body>
                 <Providers>
-                    <SidebarProvider></SidebarProvider>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset className="flex h-screen flex-col">
+                            <header className="bg-background sticky-top z-10 border-b px-4 py-2">
+                                <div className="flex shrink-0 grow items-center gap-2">
+                                    <SidebarTrigger className="-ml-1" />
+                                    <Separator
+                                        orientation="vertical"
+                                        className="mr-2 data-[orientation=vertical]:h-4"
+                                    />
+                                    <Breadcrumb>
+                                        <BreadcrumbList>
+                                            <BreadcrumbItem>
+                                                <BreadcrumbPageClient />
+                                            </BreadcrumbItem>
+                                        </BreadcrumbList>
+                                    </Breadcrumb>
+                                </div>
+                            </header>
+                            <main className="flex-1 overflow-y-auto">{children}</main>
+                            {/* <SoundBar /> */}
+                        </SidebarInset>
+                    </SidebarProvider>
                     <Toaster />
-                    {children}
                 </Providers>
             </body>
         </html>
