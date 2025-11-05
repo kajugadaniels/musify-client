add08e69e718cbf5ace6a23357741387955aeb13
kajugadaniels <kajugadaniels@gmail.com>
2025-11-05 22:56:22 +0200

    Setup auth view

--- src/app/(auth)/auth/[path]/page.tsx
+++ src/app/(auth)/auth/[path]/page.tsx
@@ -1,18 +1,16 @@
-import { AuthView } from "@daveyplate/better-auth-ui"
-import { authViewPaths } from "@daveyplate/better-auth-ui/server"
-
-export const dynamicParams = false
+import { authViewPaths } from "@daveyplate/better-auth-ui/server";
+import { AuthView } from "./view";
 
 export function generateStaticParams() {
-    return Object.values(authViewPaths).map((path) => ({ path }))
+    return Object.values(authViewPaths).map((pathname) => ({ pathname }));
 }
 
-export default async function AuthPage({ params }: { params: Promise<{ path: string }> }) {
-    const { path } = await params
-    
-    return (
-        <main className="container flex grow flex-col items-center justify-center self-center p-4 md:p-6">
-            <AuthView path={path} />
-        </main>
-    )
+export default async function AuthPage({
+    params,
+}: {
+    params: Promise<{ pathname: string }>;
+}) {
+    const { pathname } = await params;
+
+    return <AuthView pathname={pathname} />;
 }
