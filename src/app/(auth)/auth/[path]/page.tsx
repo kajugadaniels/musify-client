c15b0fadd1eb549d33c35dcb13eccaee5ab02445
kajugadaniels <kajugadaniels@gmail.com>
2025-11-05 16:47:26 +0200

    New changes

--- src/app/(auth)/auth/[path]/page.tsx
+++ src/app/(auth)/auth/[path]/page.tsx
@@ -9,7 +9,7 @@ export function generateStaticParams() {
 
 export default async function AuthPage({ params }: { params: Promise<{ path: string }> }) {
     const { path } = await params
-
+    
     return (
         <main className="container flex grow flex-col items-center justify-center self-center p-4 md:p-6">
             <AuthView path={path} />
